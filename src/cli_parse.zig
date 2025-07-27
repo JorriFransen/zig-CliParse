05dca23b294fcfcfedea2106c2a37d84f9403d25
Jorri Fransen <jorrifransen@gmail.com>
2025-07-27 22:32:29 +0200

    Parsing the first enum option.

--- src/cli_parse.zig
+++ src/cli_parse.zig
@@ -3,61 +3,53 @@ const log = std.log.scoped(.cli_parse);
 
 const Allocator = std.mem.Allocator;
 
+const assert = std.debug.assert;
+
 const Option = struct {
     name: [:0]const u8,
     short: ?u8,
 
     type: type,
-    value: union(enum) {
-        bool: bool,
-        int: i64,
-        uint: u64,
-        float: f32,
-        @"enum": u64,
-    },
+    default_value_ptr: ?*const anyopaque,
 };
 
 pub fn option(default: anytype, name: [:0]const u8, short: ?u8) Option {
     const ValueType = @TypeOf(default);
-    const ValueTypeInfo = @typeInfo(ValueType);
 
     const result = Option{
         .name = name,
         .short = short,
         .type = ValueType,
-        .value = switch (ValueTypeInfo) {
-            else => @compileError(std.fmt.comptimePrint("Invalid option type '{s}'", .{@tagName(ValueTypeInfo)})),
-
-            .bool => .{ .bool = default },
-            .int => |int| if (int.signedness == .signed) .{ .int = default } else .{ .uint = default },
-            .float => .{ .float = default },
-            .@"enum" => .{ .@"enum" = @intFromEnum(default) },
-        },
+        .default_value_ptr = &default,
     };
 
     return result;
 }
 
 pub fn OptionsStruct(comptime options: []const Option) type {
-    const fields: [options.len]std.builtin.Type.StructField = blk: {
-        var _fields: [options.len]std.builtin.Type.StructField = undefined;
-        inline for (options, &_fields) |opt, *field| {
+    const fields: [options.len + 1]std.builtin.Type.StructField = blk: {
+        var _fields: [options.len + 1]std.builtin.Type.StructField = undefined;
+        var short_names: [options.len]?u8 = undefined;
+
+        inline for (options, _fields[0..options.len], &short_names) |opt, *field, *sname| {
             field.* = .{
                 .name = opt.name,
                 .type = opt.type,
                 .alignment = @alignOf(opt.type),
                 .is_comptime = false,
-
-                // TODO: Do this in fn option, store the default_value_ptr in the Option struct
-                .default_value_ptr = pblk: switch (opt.value) {
-                    .bool => |b| break :pblk &b,
-                    .int => |i| break :pblk &i,
-                    .uint => |u| break :pblk &u,
-                    .float => |f| break :pblk &f,
-                    .@"enum" => |e| break :pblk &e,
-                },
+                .default_value_ptr = opt.default_value_ptr,
             };
+
+            sname.* = opt.short;
         }
+
+        _fields[options.len] = .{
+            .name = "__short_names",
+            .type = [options.len]?u8,
+            .alignment = @alignOf([options.len]?u8),
+            .is_comptime = false,
+            .default_value_ptr = &short_names,
+        };
 
         break :blk _fields;
     };
@@ -70,7 +62,137 @@ pub fn OptionsStruct(comptime options: []const Option) type {
     } });
 }
 
-pub fn parse(comptime OptStruct: type, allocator: Allocator) OptStruct {
+pub fn parse(comptime OptStruct: type, allocator: Allocator, tmp_allocator: Allocator) !OptStruct {
     _ = allocator;
-    unreachable;
+
+    var result: OptStruct = .{};
+
+    var arg_it = std.process.argsWithAllocator(tmp_allocator) catch @panic("OOM");
+
+    const opt_info = @typeInfo(OptStruct);
+    assert(opt_info == .@"struct");
+
+    // First argument is exe path
+    _ = arg_it.skip();
+
+    while (arg_it.next()) |arg| {
+        var token = arg;
+        const field_name: []const u8 = blk: {
+            if (std.mem.startsWith(u8, token, "--")) {
+                var name: []const u8 = undefined;
+
+                if (std.mem.indexOf(u8, token, "=")) |idx| {
+                    name = token[2..idx];
+                    token = token[idx + 1 ..];
+                } else {
+                    name = token[2..];
+                    token = "";
+                }
+
+                break :blk name;
+            } else if (std.mem.startsWith(u8, token, "-")) {
+                unreachable;
+            }
+
+            unreachable;
+        };
+
+        // handle 'name =value', 'name= value', 'name = value'
+        if (token.len == 0) {
+            token = arg_it.next() orelse return error.ExpectedValue;
+
+            if (std.mem.startsWith(u8, token, "=")) {
+                token = token[1..];
+            }
+
+            // handle 'name = value'
+            if (token.len == 0) {
+                token = arg_it.next() orelse return error.ExpectedValue;
+            }
+        }
+
+        log.debug("field_name: '{s}'", .{field_name});
+        log.debug("token: '{s}'", .{token});
+
+        inline for (opt_info.@"struct".fields[0 .. opt_info.@"struct".fields.len - 1]) |field| {
+            if (std.mem.eql(u8, field_name, field.name)) {
+                @field(result, field.name) = switch (@typeInfo(field.type)) {
+                    else => @compileError(std.fmt.comptimePrint("Unhandled type '{s}'", .{@typeName(field.type)})),
+                    .bool => unreachable,
+                    .int => unreachable,
+                    .float => unreachable,
+                    .@"enum" => std.meta.stringToEnum(field.type, token) orelse {
+                        log.err("Invalid enum value '{s}'", .{token});
+                        return error.InvalidEnumValue;
+                    },
+                };
+
+                break;
+            }
+        }
+    }
+
+    return result;
+
+    // // Combine all into a single string
+    // var first = true;
+    // var args = std.ArrayList(u8).init(tmp_allocator);
+    // while (arg_it.next()) |arg| {
+    //     if (first) {
+    //         first = false;
+    //     } else {
+    //         try args.append(' ');
+    //     }
+    //     try args.appendSlice(arg);
+    // }
+    //
+    // const opt_info = @typeInfo(OptStruct);
+    // assert(opt_info == .@"struct");
+    //
+    //
+    // // TODO: Don't think we need to tokenize?
+    // var it = std.mem.tokenizeAny(u8, args.items, &std.ascii.whitespace);
+    //
+    // while (it.next()) |token| {
+    //     if (std.mem.startsWith(u8, token, "--")) {
+    //         const name = token[2..];
+    //
+    //         // Last field is __short_names
+    //         inline for (opt_info.@"struct".fields[0 .. opt_info.@"struct".fields.len - 1]) |field| {
+    //             if (std.mem.startsWith(u8, name, field.name)) {
+    //                 log.debug("Matched option: {s}", .{field.name});
+    //
+    //                 const next = if (name.len == field.name.len)
+    //                     it.next() orelse return error.ExpectedValue
+    //                 else
+    //                     name[field.name.len..];
+    //
+    //                 log.debug("next: '{s}'", .{next});
+    //                 var value = if (std.mem.startsWith(u8, next, "="))
+    //                     next[1..]
+    //                 else
+    //                     next;
+    //
+    //                 if (value.len == 0)
+    //                     value = it.next() orelse return error.ExpectedValue;
+    //
+    //                 log.debug("value: '{s}'", .{value});
+    //
+    //                 @field(result, field.name) = switch (@typeInfo(field.type)) {
+    //                     else => @compileError(std.fmt.comptimePrint("Unhandled type '{s}'", .{@typeName(field.type)})),
+    //                     .@"enum" => std.meta.stringToEnum(field.type, value) orelse {
+    //                         log.err("Invalid enum value '{s}'", .{value});
+    //                         return error.InvalidEnumValue;
+    //                     },
+    //                     .bool => unreachable,
+    //                     .int => unreachable,
+    //                     .float => unreachable,
+    //                 };
+    //                 break;
+    //             }
+    //         }
+    //     } else if (std.mem.startsWith(u8, token, "-")) {}
+    // }
+    //
+    // return result;
 }
